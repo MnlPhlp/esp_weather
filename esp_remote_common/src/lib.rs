174f4e09f6bfcf72d2d0ebddb3742cc0748a3a2f
MnlPhlp <online@philipp-manuel.de>
2023-06-06 19:34:27 +0200

    started adding ble stuff in firmware

---
+++ esp_remote_common/src/lib.rs
@@ -0,0 +1,2 @@
+pub const SERVICE_UUID: &str = "033214d2-0ff0-4cba-814e-c5074c1ad00c";
+pub const CMD_UUID: &str = "ac6744a7-77f3-43e9-b3c8-9955ac6bb0d4";
--- esp_remote_firmware/src/ble.rs
+++ esp_remote_firmware/src/ble.rs
@@ -1,6 +1,7 @@
 use std::fmt::Error;
 
-use esp32_nimble::BLEDevice;
+use esp32_nimble::{utilities::BleUuid, uuid128, BLEDevice, NimbleProperties};
+use esp_remote_common::{CMD_UUID, SERVICE_UUID};
 
 pub fn start_advertising(ble: &'static BLEDevice) {
     log::info!("starting advertising");
@@ -10,6 +11,20 @@ pub fn start_advertising(ble: &'static BLEDevice) {
 
 pub(crate) fn setup() -> Result<(), Error> {
     let ble_device = BLEDevice::take();
+    let service = ble_device
+        .get_server()
+        .create_service(BleUuid::from_uuid128_string(SERVICE_UUID).unwrap());
+    println!("created service");
+    let mut service = service.lock();
+    let charac = service.create_characteristic(
+        BleUuid::from_uuid128_string(CMD_UUID).unwrap(),
+        NimbleProperties::READ | NimbleProperties::WRITE | NimbleProperties::WRITE_NO_RSP,
+    );
+    println!("Created charac");
+    charac.lock().on_write(move |val, _| {
+        println!("Wrote {:?}", val);
+    });
     start_advertising(ble_device);
+    println!("Started advertising");
     return Ok(());
 }
